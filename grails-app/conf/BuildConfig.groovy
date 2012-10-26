108bff60fd9e30e197766785a89ec98520158f44
Dan Durkin <ddurkin@broadinstitute.org>
2012-10-26 16:57:28 -0400

    updated release plugin to 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -45,7 +44,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion", ":release:2.0.3", ":rest-client-builder:1.0.2") {
+        build(":tomcat:$grailsVersion", ":release:2.0.4", ":rest-client-builder:1.0.2") {
             export = false
         }
 
